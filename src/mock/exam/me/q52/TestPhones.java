35ebe75a4e277248191fdb129e8ccd09ce491e6f
andersonocandido@gmail.com <andersonocandido@gmail.com>
2018-11-27 17:14:27 -0200

    Add Exam 52

---
+++ src/mock/exam/me/q52/TestPhones.java
@@ -0,0 +1,46 @@
+package mock.exam.me.q52;
+
+/*
+ * Page: 561 (592 in pdf)
+ * 
+ * ME-Q52) What is the output of the following code? (Select 1 option.)
+ * 
+ * Correct answer: b
+ */
+
+// a - wrong
+// Call-Phone
+// Call-Phone
+
+// b
+// Call-Phone
+// Call-SmartPhone
+
+// c - wrong
+// Call-Phone
+// null
+
+// d - wrong
+// null
+// Call-SmartPhone
+
+class Phone {
+	void call() {
+		System.out.println("Call-Phone");
+	}
+}
+
+class SmartPhone extends Phone {
+	void call() {
+		System.out.println("Call-SmartPhone");
+	}
+}
+
+class TestPhones {
+	public static void main(String[] args) {
+		Phone phone = new Phone();
+		Phone smartPhone = new SmartPhone();
+		phone.call();
+		smartPhone.call();
+	}
+}
