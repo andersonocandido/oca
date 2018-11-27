f2715fc35808d4b698f65a8aa6e8a83147414197
andersonocandido@gmail.com <andersonocandido@gmail.com>
2018-11-27 15:13:57 -0200

    Add Exam 42

---
+++ src/mock/exam/me/q42/EIf.java
@@ -0,0 +1,43 @@
+package mock.exam.me.q42;
+
+/*
+ * Page: 558 (589 in pdf)
+ * 
+ * ME-Q42) What is the output of the following code? (Select 1 option.)
+ * 
+ * Correct answer: e
+ */
+
+// a - wrong
+// The class will print true.
+
+// b - wrong
+// The class will print false.
+
+// c - wrong
+// The class will print true if the if condition is changed to boolean == true.
+
+// d - wrong
+// The class will print true if the if condition is changed to boolean != true.
+
+// e
+// The class won't compile.
+
+// f - wrong
+// Runtime exception.
+
+class EIf {
+	public static void main(String[] args) {
+		// e
+		// uncomment to see
+
+		// bool boolean = false;
+		// do {
+		// if(boolean = true)
+		// System.out.println("true");
+		// else
+		// System.out.println("false");
+		// }
+		// while (3.3 + 4.7 > 8);
+	}
+}
