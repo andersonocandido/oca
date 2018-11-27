5c732114dff3a511fe3633a70a140d9cff7501da
andersonocandido@gmail.com <andersonocandido@gmail.com>
2018-11-27 16:58:55 -0200

    Add Exam 50

---
+++ src/mock/exam/me/q50/Op.java
@@ -0,0 +1,40 @@
+package mock.exam.me.q50;
+
+/*
+ * Page: 561 (592 in pdf)
+ * 
+ * ME-Q49) What is the output of the following code? (Select 1 option.)
+ * 
+ * Correct answer: d
+ */
+
+// a - wrong
+// 100
+
+// b - wrong
+// 101
+
+// c - wrong
+// 102
+
+// d
+// Code fails to compile.
+
+// e - wrong
+// No output is produced.
+
+import java.util.function.Predicate;
+
+class Op {
+	public static void main(String... args) {
+		// uncomment to see
+		// d
+
+		// int a = 0;
+		// int b = 100;
+		// Predicate<Integer> compare = (var) -> var++ = 10;
+		// if (!b++ > 100 && compare.test(a)) {
+		// System.out.println(a + b);
+		// }
+	}
+}
