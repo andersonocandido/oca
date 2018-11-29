fd0b5466f45739f6aecb72bfae8ea1c4167e83ca
andersonocandido@gmail.com <andersonocandido@gmail.com>
2018-11-29 15:57:56 -0200

    Add Exam 56.

---
+++ src/mock/exam/me/q56/Shopping.java
@@ -0,0 +1,34 @@
+package mock.exam.me.q56;
+
+/*
+ * Page: 564 (595 in pdf)
+ * 
+ * ME-Q56) How many times do you think you can shop with the following code
+ * (that is, what's the output of the following code)? (Select 1 option.)
+ * 
+ * Correct answer: d
+ */
+
+// a - wrong
+// The code prints enjoying shopping once.
+
+// b - wrong
+// The code prints enjoying shopping twice.
+
+// c - wrong
+// The code prints enjoying shopping in an infinite loop.
+
+// d
+// The code fails to compile.
+
+class Shopping {
+	public static void main(String[] args) {
+		// uncomment to see
+		// d
+
+		// boolean bankrupt = false;
+		// do System.out.println("enjoying shopping");
+		// bankrupt = false;
+		// while (!bankrupt);
+	}
+}
